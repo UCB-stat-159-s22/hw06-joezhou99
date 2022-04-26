a12ff58c4d002d78f804f65451cd80b01bd30b27
Joe Zhou <joezhou1027@gmail.com>
2022-04-26 09:56:40 +0000

    added util plotting functions

--- ligotools/__init__.py
+++ ligotools/__init__.py
@@ -5,3 +5,5 @@ The ligotools package.
 __version__ = "0.2"
 
 from .readligo import *
+
+from .utils import *
--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,5 +1,27 @@
 from ligotools import readligo as rl
 import numpy as np
+import json
 
-def test_dq_channel_to_seglist():
+
+
+fnjson = "data/BBH_events_v3.json"
+events = json.load(open(fnjson,"r"))
+eventname = 'GW150914' 
+event = events[eventname]
+fn_H1 = "data/" + event['fn_H1'] 
+strain_H1, time_H1, chan_dict_H1 = rl.loaddata(fn_H1, 'H1')
+
+def test_dim_channel_to_seglist():
     assert (len(rl.dq_channel_to_seglist(np.array([1,1,1,1])))==1)
+    
+def test_type_channel_to_seglist():    
+    assert (type(rl.dq_channel_to_seglist(np.array([1,1,1,1])))==list)
+    
+def test_dim_loaddata():
+    assert (len(time_H1)==len(strain_H1))
+    assert (len(strain_H1)==131072)
+    assert (len(chan_dict_H1)==13)
+    
+def test_type_loaddata():   
+    assert (type(rl.loaddata(fn_H1, 'H1'))==tuple)
+    
